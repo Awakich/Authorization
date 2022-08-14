1321b50a2b1c61e6325b8a760cd4b6091c3e6e16
Artem <artem.bqpower@gmail.com>
2022-08-14 17:08:07 +0300

    add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,21 @@
+const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
-    
+    entry: "./src/app.js",
+    output: {
+        filename: "bundle.[chunkhash].js",
+        path: path.resolve(__dirname, 'public')
+    },
+    devServer:{
+        port: 8800
+    },
+
+    plugins: [
+        new HTMLPlugin({
+            template: "./src/index.html"
+        }),
+        new CleanWebpackPlugin()
+    ]
 }
